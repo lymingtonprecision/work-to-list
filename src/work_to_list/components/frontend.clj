80ea97ac6cbc644502fbc300eebaec12c5af72bc
Adam Harper <github@adam-harper.com>
2016-03-24 09:01:14 +0000

    Make page title plural (List -> Lists)

--- src/work_to_list/components/frontend.clj
+++ src/work_to_list/components/frontend.clj
@@ -72,7 +72,7 @@
   (with-base-url uri-prefix
     (html5
      [:head
-      [:title "Work To Lists"]
+      [:title "Work To List"]
       [:meta {:charset "UTf-8"}]
       [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
       [:link {:rel "icon" :href (url (asset "/favicon.ico")) :type "image/x-icon"}]
