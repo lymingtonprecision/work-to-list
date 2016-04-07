483f8afd1b2f15db7936c3898df4b9addc084362
Adam Harper <github@adam-harper.com>
2016-04-07 08:12:10 +0100

    Version 1.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject work-to-list "1.0.2"
+(defproject work-to-list "1.0.3-SNAPSHOT"
   :description "LPE IFS DBR Work To List display"
   :url "https://github.com/lymingtonprecision/work-to-list"
   :license {:name "Eclipse Public License"
